28a34db8a426ea532fe8975e628a89846cc40bc3
key46 <duythien0912@gmail.com>
2019-02-22 00:13:11 +0700

    fix http to https

--- src/untils/fetchData.ts
+++ src/untils/fetchData.ts
@@ -5,7 +5,7 @@ const fetch = fetchImport.default || fetchImport;
 export const fetchData = config => {
   const host =
     process.env.NODE_ENV === "production"
-      ? "http://mydailyinfobackend.now.sh/"
+      ? "https://mydailyinfobackend.now.sh/"
       : "http://localhost:4000/";
   return fetch(host, config);
 };
